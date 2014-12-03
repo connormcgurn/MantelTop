68f691c2ee0928fb131eff83041f570e9a487df0
JBarna <joel.tbarna@gmail.com>
2014-12-02 20:23:47 -0500

    Added lots of css changes

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,13 +1,26 @@
 @extends('layouts.default')
 
 @section('content')
-	
-      <h1>Welcome to Mantel Top Photography</h1>
 
-      @if(Auth::check())
+      
+
+    <div class="row frontThumbs">
+  <div class="col-xs-12 col-sm-6">
+    <a href="/PortraitsandModeling" class="thumbnail">
+      <img class="grow" src="/assets/images/Portraits_and_modeling_half.jpg" alt="Portraits and Modeling" >
+    </a>
+  </div>
+        
+    <div class="col-xs-12 col-sm-6">
+    <a href="/browseRacesCont" class="thumbnail">
+      <img class="grow" src="/assets/images/race_and_sports_banner_half.jpg" alt="Browse Races">
+    </a>
+  </div>
+</div>
+    
+        @if(Auth::check())
       <h2>Logged In</h2>
       @else
       <h2>Not Logged In</h2>
       @endif
-    
 @stop
