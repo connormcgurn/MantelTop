35be1cd9a7a59441bb1a757f59d0444ed42fe7e5
Brandon Schwarzer <bpq29@wildcats.unh.edu>
2014-12-10 23:41:12 -0500

    basic page info (not linked yet)

--- app/views/racesandsports.blade.php
+++ app/views/racesandsports.blade.php
@@ -2,12 +2,11 @@
 
 @section('content')
 
-<h1>Races and Sports</h1>
-<img style="float:right;box-shadow: 0 0 15px black" src="assets/images/race_sports_page.jpg" width="290px" height="341" Hspace="10" Vspace="5">
-<p>We know how hard you trained for that race, let us make it a memory! When you see Mantel Top Photography at a race or sporting event, you can expect complete coverage from start to finish. Other companies seem to have cluttered websites, difficult photo viewing systems, and very high prices, all for mediocre pictures. With Mantel Top Photography, you can always expect ease of use, reliability and quality images, all at very competitive prices.</p>
-<p>What sets us apart from other photography companies is that we recognize the growing influence and ease of use of technology today. That's why we're focusing on a completely digital age. When you purchase pictures through our website, you're buying high quality digital image files to use any way you would like. Post them to Facebook, make prints at your preferred photo printer, share with your family! We'll make it easy. And while other sites discourage the sales of digital images and charge prices such as $30, $40, and even $50 a picture, Mantel Top Photography offers professional digital purchases for just $10 a picture.</p>
- <a href="#" class="btn btn-lg btn-primary">Browse Races</a>
- <p>If you're a Race Director and would like further information about having Mantel Top Photography cover your next event at no cost to you, click here.</p>
- <p>Custom prints are available by contacting our Support email and are negotiated on a consultation basis.</p>
- 
+
+
+
+
+
+
+
 @stop
